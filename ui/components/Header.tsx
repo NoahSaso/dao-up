d926966db83388daa79d22b3e9266f7ce4d3ae6f
Noah Saso <noahsaso@gmail.com>
2022-01-30 13:12:23 -0800

    simplified header on small screens

--- ui/components/Header.tsx
+++ ui/components/Header.tsx
@@ -1,4 +1,4 @@
-import classnames from "classnames"
+import cn from "classnames"
 import Image from "next/image"
 import Link from "next/link"
 import { useRouter } from "next/router"
@@ -10,19 +10,31 @@ interface NavItemProps {
   href: string
   label: string
   action?: boolean
+  className?: string
 }
-const NavItem: FC<NavItemProps> = ({ href, label, action }) => {
+const NavItem: FC<NavItemProps> = ({ href, label, action, className }) => {
   const { pathname } = useRouter()
 
   return action ? (
-    <ButtonLink href={href} label={label} outline className="ml-8" />
+    <ButtonLink
+      href={href}
+      label={label}
+      outline
+      className={cn("ml-8", className)}
+    />
   ) : (
     <Link href={href}>
       <a
-        className={classnames("ml-8", "hover:text-green", "transition", {
-          "py-2 px-4 rounded-full border border-green text-green": action,
-          "text-green": pathname === href,
-        })}
+        className={cn(
+          "ml-8",
+          "hover:text-green",
+          "transition",
+          {
+            "py-2 px-4 rounded-full border border-green text-green": action,
+            "text-green": pathname === href,
+          },
+          className
+        )}
       >
         {label}
       </a>
@@ -36,14 +48,24 @@ export const Header: FC = () => (
       <Link href="/">
         <a className="flex flex-row items-center">
           <Image src="/images/logo.svg" alt="logo" width={52} height={30} />
-          <h1 className="text-2xl ml-4">DAO Up!</h1>
+          <h1 className={cn("text-2xl ml-4", "hidden sm:inline-block")}>
+            DAO Up!
+          </h1>
         </a>
       </Link>
 
       <div className="flex flex-row items-center">
         <NavItem href="/campaigns" label="Campaigns" />
-        <NavItem href="/#faq" label="FAQ" />
-        <NavItem href="/create" label="Create Campaign" action />
+
+        <NavItem href="/#faq" label="FAQ" className="hidden sm:inline-block" />
+
+        <NavItem
+          href="/create"
+          label="Create Campaign"
+          action
+          className="hidden sm:inline-block"
+        />
+        <NavItem href="/create" label="Create" action className="sm:hidden" />
       </div>
     </nav>
   </header>
