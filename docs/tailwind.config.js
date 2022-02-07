05d864f878e28a3f1beec8f00600980b723f9a0a
Zeke Medley <zekemedley@gmail.com>
2022-02-06 21:14:26 -0800

    Set up documentation and document campaign creation

---
+++ docs/tailwind.config.js
@@ -0,0 +1,33 @@
+const { fontFamily, screens } = require("tailwindcss/defaultTheme")
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./services/**/*.{js,ts,jsx,tsx}",
+    "./helpers/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      ...fontFamily,
+      sans: ["DM Sans", ...fontFamily.sans],
+    },
+    colors: {
+      light: "#f4f4f4",
+      dark: "#0e0e0e",
+      green: "#c1eb7c",
+      orange: "#daa27a",
+      card: "#1c1d1a",
+      placeholder: "#666666",
+    },
+    screens: {
+      xs: "420px",
+      ...screens,
+    },
+    extend: {
+      animation: {
+        "pulse-fast": "pulse 1.2s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      },
+    },
+  },
+}
---
+++ docs/theme.config.js
@@ -0,0 +1,67 @@
+export default {
+  github: 'https://github.com/NoahSaso/dao-up',
+  docsRepositoryBase: 'https://github.com/NoahSaso/dao-up/tree/main/docs',
+  titleSuffix: ' – DAO Up!',
+  logo: (
+    <>
+      <span className="mr-2 font-extrabold hidden md:inline">DAO Up!</span>
+      <span className="text-gray-600 font-normal hidden md:inline">
+        The crowdfunding platform for DAOs.
+      </span>
+    </>
+  ),
+  head: (
+    <>
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="Content-Language" content="en" />
+      <meta name="description" content="DAO Up! The crowdfunding platform for DAOs" />
+      <meta name="og:description" content="DAO Up! The crowdfunding platform for DAOs" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image" content="https://daoup.zone/_next/image?url=%2Fimages%2Flogo.svg&w=128&q=75" />
+      <meta name="twitter:site:domain" content="https://daoup.zone" />
+      <meta name="twitter:url" content="https://daoup.zone" />
+      <meta name="og:title" content="DAO Up! The crowdfunding platform for DAOs" />
+      <meta name="og:image" content="https://daoup.zone/_next/image?url=%2Fimages%2Flogo.svg&w=128&q=75" />
+      <meta name="apple-mobile-web-app-title" content="DAO Up!" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-icon-180x180.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/android-icon-192x192.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/favicon-96x96.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+    </>
+  ),
+  search: true,
+  prevLinks: true,
+  nextLinks: true,
+  footer: true,
+  footerEditLink: 'Edit this page on GitHub',
+  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  unstable_faviconGlyph: '🪐',
+}
