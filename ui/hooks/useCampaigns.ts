2c7552c88b9566c538cf20d680f2abb0408d7f50
Noah Saso <noahsaso@gmail.com>
2022-02-10 11:50:47 -0800

    Added keyword filtering system (e.g. status:pending).

--- ui/hooks/useCampaigns.ts
+++ ui/hooks/useCampaigns.ts
@@ -5,6 +5,22 @@ import { useRecoilValue, waitForAll } from "recoil"
 import { campaignsFromResponses } from "../services/campaigns"
 import { escrowContractAddresses, fetchCampaign } from "../state/campaigns"
 
+enum Filter {
+  Status = "status",
+}
+type FilterType = `${Filter}`
+
+type FilterFunction = (campaign: Campaign) => boolean
+const filterFnMakers: Record<FilterType, (value: string) => FilterFunction> = {
+  status: (value) => (c) => c.status === value,
+}
+const filterKeys = Object.keys(filterFnMakers)
+// Matches `key{whitespace}:{whitespace}value` or `key{whitespace}:{whitespace}"value"`
+const filterRegex = new RegExp(
+  `(${filterKeys.join("|")})\\s*:\\s*([^\\s]+|"[^"]+")`,
+  "gi"
+)
+
 let campaignsFilterId = 0
 export const useCampaigns = (
   filter?: string,
@@ -27,29 +43,62 @@ export const useCampaigns = (
 
   useEffect(() => {
     const updateCampaigns = async () => {
-      if (!filter)
-        return setCampaigns(
-          campaignsFromResponses(
-            campaignResponses,
-            includeHidden,
-            includePending
-          )
-        )
-
       setFiltering(true)
       setFilterError(null)
+
+      let relevantCampaigns = campaignsFromResponses(
+        campaignResponses,
+        includeHidden,
+        includePending
+      )
+
+      if (!filter) {
+        setFiltering(false)
+        return setCampaigns(relevantCampaigns)
+      }
+
+      // Extract filter keys from filter string.
+      const filterKeyMatches = filter.match(filterRegex) ?? []
+      const activeFilterFns: FilterFunction[] = []
+      let search = filter
+      for (const match of filterKeyMatches) {
+        const key = match.split(":")[0].trim()
+        const value = match
+          .split(":")
+          .slice(1)
+          .join(":")
+          .trim()
+          // Remove quotes from ends of value if present.
+          .replace(/^"/, "")
+          .replace(/"$/, "")
+          .trim()
+
+        if (!(key in filterFnMakers)) continue
+
+        // Make filter function and add to active filters.
+        const filterFn = filterFnMakers[key as FilterType]
+        activeFilterFns.push(filterFn(value))
+
+        // Remove filter key/value from search string.
+        search = search.replace(match, "").trim()
+      }
+
+      // Filter if any filters are active.
+      if (activeFilterFns.length)
+        relevantCampaigns = relevantCampaigns.filter((c) =>
+          activeFilterFns.every((fn) => fn(c))
+        )
+
+      if (!search) {
+        setFiltering(false)
+        return setCampaigns(relevantCampaigns)
+      }
 
       try {
         let id = ++campaignsFilterId
 
-        const relevantCampaigns = campaignsFromResponses(
-          campaignResponses,
-          includeHidden,
-          includePending
-        )
-
         const filtered = (
-          await fuzzysort.goAsync(filter, relevantCampaigns, {
+          await fuzzysort.goAsync(search, relevantCampaigns, {
             keys: ["name", "description"],
             allowTypo: true,
           })
