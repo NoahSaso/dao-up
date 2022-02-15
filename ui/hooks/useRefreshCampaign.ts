f994d32cb2d30f51dc7640777bbbbbe1ff281f42
Noah Saso <noahsaso@gmail.com>
2022-02-15 00:15:53 -0800

    Refresh governance token balance when campaign refreshes.

--- ui/hooks/useRefreshCampaign.ts
+++ ui/hooks/useRefreshCampaign.ts
@@ -9,13 +9,18 @@ export const useRefreshCampaign = (campaign: Campaign | null) => {
   const setFundingTokenAddressBalanceId = useSetRecoilState(
     tokenAddressBalanceId(campaign?.fundingToken.address)
   )
+  const setGovTokenAddressBalanceId = useSetRecoilState(
+    tokenAddressBalanceId(campaign?.govToken.address)
+  )
 
   const refreshCampaign = () => {
     if (!campaign) return
     // Refresh campaign data.
     setCampaignStateId((id) => id + 1)
-    // Refresh funding token balances (in the case of contributing or refunding, this will update the wallet balance displayed).
+    // Refresh funding token balance (in the case of contributing or refunding, this will update the wallet balance displayed).
     setFundingTokenAddressBalanceId((id) => id + 1)
+    // Refresh governance token balance (in the case of joining the DAO, this will update the wallet balance displayed).
+    setGovTokenAddressBalanceId((id) => id + 1)
   }
 
   return { refreshCampaign }
